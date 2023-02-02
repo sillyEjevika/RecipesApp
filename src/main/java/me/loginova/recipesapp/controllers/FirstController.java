06f16cc6c37adbddeb99e93928d1a74b6f36a698
sillyEjevika <Doroteia_Vika@mail.ru>
2023-02-02 23:26:19 +0300

    приложение рецептов

---
+++ src/main/java/me/loginova/recipesapp/controllers/FirstController.java
@@ -0,0 +1,16 @@
+package me.loginova.recipesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping( "/")
+    public String appWork() {
+        return "Приложение запущено";
+    }
+    @GetMapping( "/info")
+    public String info() {
+        return "Имя ученика: Логинова Виктория <br/> Проект: Рецепты <br/> Дата создания проекта: 02.02.2023 <br/> Описание проекта: вкусные рецепты на каждый день";
+    }
+}
